28fc34a18df504e52f4131f14d35a65fae9cdcfe
Jan Halama <mail@janhalama.cz>
2022-05-20 13:10:16 +0200

    refactor: inline CSS on error page

--- src/pages/500.js
+++ src/pages/500.js
@@ -1,39 +1,30 @@
-const styles = {
-  PAGE: 'h-screen bg-gray-900 antialiased',
-  ALIGNER: 'container h-full mx-auto flex justify-center items-center',
-  CARD: 'max-w-prose mx-auto p-10 bg-white rounded-md',
-  H: 'font-black text-xl p-0 m-0 pb-3',
-  P: 'font-regular pb-3 leading-7',
-  UL: 'list-disc list-inside text-blue-600',
-  LI: 'mb-2',
-  FOOTER: 'w-full text-center mt-8',
-};
-
 import Head from 'next/head';
 import Image from 'next/image';
 import logo from '../public/logo.png';
 
 export const Hooshek500 = () => (
-  <div className={styles.PAGE}>
+  <div className="h-screen bg-gray-900 antialiased">
     <Head>
       <title>Chyba aplikace | Sokol Skuhrov</title>
     </Head>
 
-    <div className={styles.ALIGNER}>
+    <div className="container h-full mx-auto flex justify-center items-center">
       <div>
-        <div className={styles.CARD}>
-          <h1 className={styles.H}>Došlo k chybě aplikace!</h1>
-          <p className={styles.P}>
+        <div className="max-w-prose mx-auto p-10 bg-white rounded-md">
+          <h1 className="font-black text-xl p-0 m-0 pb-3">
+            Došlo k chybě aplikace!
+          </h1>
+          <p className="font-regular pb-3 leading-7">
             Zkuste webovou stránku obnovit. Pokud k chybě dochází opakovaně:
           </p>
-          <ul className={styles.UL}>
-            <li className={styles.LI}>
+          <ul className="list-disc list-inside text-blue-600">
+            <li className="mb-2">
               <a href="mailto:mail@janhalama.cz">Kontaktujte vývojáře</a>
             </li>
           </ul>
         </div>
 
-        <div className={styles.FOOTER}>
+        <div className="w-full text-center mt-8">
           <Image src={logo} alt="Sokol Skuhrov" width="40" height="40" />
         </div>
       </div>
