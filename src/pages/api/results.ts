198b6316af63c7afd346be70b2b2284243f073b3
Jan Halama <mail@janhalama.cz>
2022-05-24 15:15:34 +0200

    chore: add error logging in api handler

--- src/pages/api/results.ts
+++ src/pages/api/results.ts
@@ -49,6 +49,7 @@ export default async function handler(
 
     res.status(200).json({ success: true });
   } catch (error) {
+    console.error('Unhandled exception, POST /api/results route', error);
     res.status(500).json({
       message: 'Internal server error',
     });
